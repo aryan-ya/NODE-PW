9f03fef75e8d0f49de6a119af7a1c67efbd0a5e0
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-07-14 13:23:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,34 @@ const { hostname } = require('os');
 const PORT = 3020;
 
 const server =  http.createServer((req , res) =>{
-    res.end('Node Server create by Aryan yadav')
+// home page 
+// about page
+// contact page
+// product page
+// Reast ... > error
+
+if (req.url == '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Welcome to Node js server by chirag goel')
+}else if(req.url ==  '/about'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page!')
+
+}else if(req.url  == '/contact'){
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Contact Page!')
+}else{
+    res.statusCode =500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('server error');
+}
+
+
+
+   
 });
 
 server.listen(PORT, () =>{
